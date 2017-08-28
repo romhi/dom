79c88b4f1eb7606b860c8b1bf605df0b990cd6b2
Roman Khitroff <roman.khitroff@gmail.com>
2017-08-28 16:55:06 +0300

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '178.162.199.139', port: 22, roles: [:web, :app, :db], primary: true
+server '178.162.199.139', roles: [:web, :app, :db], primary: true
 
 set :repo_url,       ' git@github.com:romhi/dom.git'
 set :application,     'dom'
